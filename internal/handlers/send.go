5dfefd524b256fe448ee120ad3676169cb1420fc
Theo Mazars <me@theomazars.com>
2026-02-15 20:09:17 +0100

    feat(http): add basic send endpoint

--- internal/handlers/send.go
+++ internal/handlers/send.go
@@ -1,7 +1,74 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/mail"
+	"strings"
+
+	"github.com/theo-mazars/postrun/internal/smtp"
+)
+
+type SendRequest struct {
+  From    string `json:"from" validate:"nonzero"`
+	To      string `json:"to" validate:"nonzero"`
+	Subject string `json:"subject" validate:"nonzero"`
+	Text    string `json:"text" validate:"nonzero"`
+}
 
 func (s *Server) SendHandler(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Hello World!"))
+  sendRequest := &SendRequest{}
+
+  err := json.NewDecoder(r.Body).Decode(&sendRequest)
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    return
+  }
+
+  email, err := sendRequest.validate()
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    fmt.Println(err)
+    return
+  }
+  if !strings.HasSuffix(email.From.Address, r.Context().Value("domain").(string)) {
+    w.WriteHeader(http.StatusUnauthorized)
+    return
+  }
+
+  sender := smtp.NewSender(r.Context().Value("domain").(string))
+  response, err := sender.Send(email)
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    fmt.Println(err)
+    return
+  }
+  if response.Code != 250 {
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write([]byte(response.Message))
+    return
+  }
+
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte(response.Message))
+}
+
+func (s *SendRequest) validate() (*smtp.Email, error) {
+  from, err := mail.ParseAddress(s.From)
+  if err != nil {
+    return nil, fmt.Errorf("from: %s", err)
+  }
+
+  to, err := mail.ParseAddress(s.To)
+  if err != nil {
+    return nil, fmt.Errorf("from: %s", err)
+  }
+
+  return &smtp.Email{
+    From: from,
+    To: to,
+    Subject: s.Subject,
+    Body: s.Text,
+  }, nil
 }
